867ef1218e76bc3ca502f7b1f5b8d8eeb2dbb141
dwikaharaprsudpulpis <dwikaharaprsudpulpis@gmail.com>
2023-07-29 10:07:51 +0800

    AdminLTE

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!-- Preloader -->
+<div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__wobble" src="{{asset('AdminLTE')}}/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60"
+        width="60">
+</div>
+
+@include('layouts.header')
+@include('layouts.navbar')
+@include('layouts.sidebar')
+@include('layouts.content')
+@include('layouts.footer')
+
+
+
+
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
